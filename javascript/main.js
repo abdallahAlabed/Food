f36286a964eac4a22fd3f6d44bd6a79c884ea31d
abdallahAlabed <abdallah.s.alabd@gmail.com>
2021-03-29 15:05:16 +0300

    btata

--- javascript/main.js
+++ javascript/main.js
@@ -1,29 +1,44 @@
 var food = prompt("Please enter the food name", "mansf,shawrma");
-
 var num = prompt("Please enter the of meals", 1);
-
 var img = '';
 
-while (!(food == "mansf" || food == "shawrma")) {
+theNamFoFood();
+document.write(img1());
 
-        food = prompt("Please enter the food name", "mansf,shawrma");
+function theNamFoFood() {
 
-}
-
-
-
-if (food == "shawrma") {
-        for (i = 0; num > i; i++) {
-
-                img += "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNxfIRq170fiMZeqzcEbH9NPSIgI18tzjEvw&usqp=CAU'>";
-        }
-
-} else if (food == "mansf") {
-        for (i = 0; num > i; i++) {
-
-                img += "<img src='https://www.tareekaa.com/wp-content/uploads/2014/12/%D9%85%D9%86%D8%B3%D9%811.jpg'>";
+        if (!(food == "mansf" || food == "shawrma")) {
+                food = prompt("Please enter the food name", "mansf,shawrma");
+                theNamFoFood();
+        } else {
+                return food;
         }
 
 }
 
-document.write(img);
+
+function img1() {
+        var img = '';
+        for (i = 0; num > i; i++) {
+                if (food == "shawrma") {
+
+
+
+
+                        img += "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNxfIRq170fiMZeqzcEbH9NPSIgI18tzjEvw&usqp=CAU'>";
+
+
+
+                } else if (food == "mansf") {
+
+
+                        img += "<img src='https://www.tareekaa.com/wp-content/uploads/2014/12/%D9%85%D9%86%D8%B3%D9%811.jpg'>";
+
+
+
+                }
+        }
+        return img;
+
+}
+
